7703ae520bf8e976e7a29f964ae3d0162e0f66f2
Sachit Baligit config --global user.name Sachit <sachitbali@yahoo.com>
2023-11-10 20:45:42 +1100

    testing a new change

--- scripts/mvn.sh
+++ scripts/mvn.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #this will excute the previous one
 sudo mvn clean install
-sudo cp /root/.buildkite-agent/builds/Devops-1/devops1/first-build/target * /home/sachit/project/
+#sudo rm -rf /home/sachit/poject/*
+sudo cp -r /root/.buildkite-agent/builds/Devops-1/devops1/first-build/target/ /home/sachit/project/
